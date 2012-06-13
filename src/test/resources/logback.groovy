9c88bb5eddf45f2d0fd68b6e2be6de5a376ef1aa
Anthony Marcar <amarcar@walmartlabs.com>
2012-06-12 22:22:45 -0700

    getting ready for sonatype oss release

--- src/test/resources/logback.groovy
+++ src/test/resources/logback.groovy
@@ -4,12 +4,12 @@ import static ch.qos.logback.classic.Level.DEBUG
 
 appender("GELF", GelfAppender) {
     facility = "logback-gelf-test"
-    graylog2ServerHost = "localhost"
+    graylog2ServerHost = "adm-grbl00.sv.walmartlabs.com"
     graylog2ServerPort = 12201
     useLoggerName = true
     graylog2ServerVersion = "0.9.6"
     chunkThreshold = 1000
-    additionalFields = [ipAddress:"_ip_address", requestId:"_request_id"]
+    additionalFields = [ipAddress:"_ip_address", requestId:"_request_id", contextName:"_context_name"]
 }
 
 root(DEBUG, ["GELF"])
