dc563646befa293757d3e004d4a771daaef58d9a
Anthony Marcar <amarcar@walmartlabs.com>
2015-03-22 19:14:22 -0700

    Added maven lein connector

---
+++ src/main/java/me/moocar/logbackgelf/GelfEncoder.java
@@ -0,0 +1,19 @@
+package me.moocar.logbackgelf;
+
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.encoder.LayoutWrappingEncoder;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.zip.GZIPOutputStream;
+
+/**
+ * Created by anthony on 3/9/15.
+ */
+public class GelfEncoder<E extends ILoggingEvent> extends LayoutWrappingEncoder<E> {
+    @Override
+    public void init(OutputStream os) throws IOException {
+        //super.init(new GZIPOutputStream(os));
+        super.init(os);
+    }
+}
---
+++ src/main/java/me/moocar/logbackgelf/SocketEncoderAppender.java
@@ -0,0 +1,7 @@
+package me.moocar.logbackgelf;
+
+/**
+ * Created by anthony on 3/8/15.
+ */
+public class SocketEncoderAppender {
+}
