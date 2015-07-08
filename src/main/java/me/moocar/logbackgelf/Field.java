14c504431faaa225eb451230730c49411be4a048
Anthony Marcar <amarcar@walmartlabs.com>
2015-07-07 21:53:05 -0700

    added staticField functionality to GelfLayout

--- src/main/java/me/moocar/logbackgelf/Field.java
+++ src/main/java/me/moocar/logbackgelf/Field.java
@@ -1,7 +1,23 @@
 package me.moocar.logbackgelf;
 
-/**
- * Created by anthony on 7/7/15.
- */
 public class Field {
+
+    private String key;
+    private String value;
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
 }
